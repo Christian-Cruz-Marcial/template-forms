b50176c86d2fbf1633e02260e980ce9d9405c511
unknown <c.cruzmarcial2001@gmail.com>
2024-01-18 17:26:53 -0600

    finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   subscriptions = ['Basic', 'advanced', 'Pro'];
   selectedSubscription = 'advanced';
+  @ViewChild('signupForm') sgnForm!: NgForm;
+  onSubmit(){
+     console.log(this.sgnForm.value)
+  }
 }
